cc3ff53b48604165b54fbf7cfb7182f42013d385
sendaitanitakeshi <t.kawauchiya@gmail.com>
2021-03-12 11:30:13 +0900

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/main_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String yurikoText = '人間は嫌いだ！！';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('石田ゆりこ'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(children: [
+                Text(model.yurikoText,
+                    style: TextStyle(
+                      fontSize: 30,
+                    )),
+                RaisedButton(
+                    child: Text('そなたは美しい。'),
+                    onPressed: () {
+                      // なんかの処理
+                      model.changeYurikoText();
+                    })
+              ]),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
